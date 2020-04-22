ddb8e0c503e92ddd79b60679669b7e127b067d18
WZJQWERT <2459604063@qq.com>
2020-04-22 18:27:53 +0800

    tijiao

---
+++ src/com/gfxy/lianXi/Ks16.java
@@ -0,0 +1,14 @@
+package com.gfxy.lianXi;
+
+public class Ks16 {
+    //*********Found**********
+    public static void main(String [] args) {
+        String s1=new String("你正在考试");
+        String s2=new String("你正在考试");
+        System.out.println(s1==s2);
+        //*********Found**********
+        System.out.println(s1.equals(s2));
+    }
+}
+
+
