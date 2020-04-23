d23f9e4e5b4abb79e681c8960adc884a0113ad4e
WZJQWERT <2459604063@qq.com>
2020-04-23 17:24:37 +0800

    提交

---
+++ src/com/gfxy/lianXi/Ks19.java
@@ -0,0 +1,16 @@
+package com.gfxy.lianXi;
+
+public class Ks19 {
+    //*********Found**********
+    public static void main(String []args) {
+        char ch = 'd';
+        //*********Found**********
+        switch(ch) {
+            case 'a': System.out.print("a");break;
+            case 'b': System.out.print("b");
+            case 'c': System.out.print("c");break;
+            //*********Found**********
+            case 'd': System.out.print("abc");
+        }
+    }
+}
